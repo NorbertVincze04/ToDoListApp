79cadd438e72ac1fa3fffce2a6f09714084c544b
EXTERNAL Vincze Norbert-Istvan (Robert Bosch SRL <vin3clj@bosch.com>
2026-04-08 13:00:05 +0300

    header template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopBarComponent],
+  imports: [RouterOutlet, TopBarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
