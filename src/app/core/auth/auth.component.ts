4a1b30c3ef3def0901950ca14635e76529b7b4e1
EXTERNAL Vincze Norbert-Istvan (Robert Bosch SRL <vin3clj@bosch.com>
2026-04-29 16:25:58 +0300

    login form ui

--- src/app/core/auth/auth.component.ts
+++ src/app/core/auth/auth.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { TasksService } from '../../shared/components/tasks/tasks.service';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrl: './auth.component.css',
 })
 export class AuthComponent {
+  constructor(private users: TasksService) {}
+  @Output() cancel = new EventEmitter<void>();
 
+  form = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    birthDay: new FormControl(new Date()),
+    gender: new FormControl<'Male' | 'Female' | 'Other'>('Male'),
+  });
+
+  onSubmit() {
+    // if (this.form.value.birthDay) {
+    //   this.users.addTask(
+    //     this.form.value.firstName!,
+    //     this.form.value.lastName!,
+    //     this.form.value.dueDate!,
+    //     this.form.value.priority!,
+    //   );
+    //   this.form.reset();
+    //   this.cancel.emit();
+    // }
+  }
+
+  onCancel() {
+    this.cancel.emit();
+  }
+
+  register() {}
 }
--- src/app/shared/components/new-task/new-task.component.ts
+++ src/app/shared/components/new-task/new-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { TasksService } from '../tasks/tasks.service';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
